897643abe6b323d0ae098fe728c48cd2f1d599b5
Zain Farooqui <zainfarooqui215@gmail.com>
2023-12-03 21:22:13 +0000

    finish part one and part two for day 3

--- 2023/Day3/part_one.py
+++ 2023/Day3/part_one.py
@@ -1,3 +1,5 @@
+import math
+
 FILE_PATH = 'input.txt'
 
 def read_files(file_path):
@@ -10,36 +12,35 @@ def read_files(file_path):
 class PartNumbersCoordCache:
     def __init__(self):
         self.cache = set()
-        
+
     def add(self, x1, x2, y):
         self.cache.add((x1, x2, y))
-        
+
     def check_cache(self, x1, x2, y):
         return (x1, x2, y) in self.cache
 
 class Schematic:
-    def __init__(self, schematic):
-        self.schematic = schematic
+    def __init__(self, input_schematic):
+        self.schematic = input_schematic
         self.cache = PartNumbersCoordCache()
-        self.part_numbers = []
-        self.n = len(schematic)
-        self.m = len(schematic[0])
-        self.symbols = set()
-        self.any = set()
-    
-    def search_coord(self, x, y):
+        self.n = len(input_schematic)
+        self.m = len(input_schematic[0])
+        self.sum_of_parts = 0
+        self.gear_ratio = 0
+
+    def __search_coord(self, x, y):
         x1 = x - 1
         x2 = x + 1
         part_number = self.schematic[y][x]
-        
-        while x1 > 0 and self.schematic[y][x1].isdigit():
+
+        while x1 >= 0 and self.schematic[y][x1].isdigit():
             part_number = self.schematic[y][x1] + part_number
             x1 -= 1
-        
+
         while x2 < self.m and self.schematic[y][x2].isdigit(): 
             part_number = part_number + self.schematic[y][x2]
             x2 += 1
-        
+
         x1 += 1
         x2 -= 1
         if self.cache.check_cache(x1, x2, y):
@@ -47,33 +48,34 @@ class Schematic:
         else:
             self.cache.add(x1, x2, y)
             return int(part_number)
-            
-    def find_adjacent_numbers(self, i, j):
+
+    def __find_adjacent_numbers(self, i, j):
+        adjacent_numbers = []
         for x in range(i-1, i+2):
             for y in range(j-1, j+2):
                 if -1 < x < self.m and -1 < y < self.n and self.schematic[y][x].isdigit():
-                    part_number = self.search_coord(x, y)
+                    part_number = self.__search_coord(x, y)
                     if part_number != -1:
-                        self.part_numbers.append(part_number)
+                        adjacent_numbers.append(part_number)
+        return adjacent_numbers
 
-    def find_all_part_numbers(self):
-        schematic = self.schematic
+    def search_schematic(self):
         n = self.n
         m = self.m
-        
+
         for y in range(n):
             for x in range(m):
-                curr_char = schematic[y][x]
-                self.any.add(curr_char)
+                curr_char = self.schematic[y][x]
                 if (not curr_char.isdigit() and curr_char != '.'):
-                    self.symbols.add(curr_char)
-                    self.find_adjacent_numbers(x, y)
-                        
-    
+                    extension = self.__find_adjacent_numbers(x, y)
+                    self.sum_of_parts += sum(extension)
+                    if curr_char == '*' and len(extension) == 2:
+                        self.gear_ratio += math.prod(extension)
+
 if __name__ == "__main__":
     data = read_files(FILE_PATH)
     schematic = Schematic(data)
-    schematic.find_all_part_numbers()
-    print(schematic.any)
-    print(schematic.symbols)
-    print(schematic.any.difference(schematic.symbols))
+    schematic.search_schematic()
+
+    print(f'The sum of all part numbers is: {schematic.sum_of_parts}')
+    print(f'The gear ratio is: {schematic.gear_ratio}')
