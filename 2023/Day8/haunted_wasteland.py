b03213777b1fba2dd51e0d3c6d99045629778018
zain <zainfarooqui215@gmail.com>
2023-12-08 15:32:05 +0000

    finish day 8 part two

--- 2023/Day8/haunted_wasteland.py
+++ 2023/Day8/haunted_wasteland.py
@@ -1,3 +1,5 @@
+import math
+
 def read_file(file_path):
 	data = []
 	with open(file_path, 'r') as file:
@@ -10,11 +12,14 @@ def read_file(file_path):
 def format_data(data):
 	instructions = data[0]
 	network = {}
+	start_nodes = []
 
 	for node in data[2:]:
+		if node[2] == 'A':
+			start_nodes.append(node[0:3])
 		network[node[0:3]] = (node[7:10], node[12:15])
 	
-	return instructions, network
+	return instructions, network, start_nodes
 
 def traverse_network(instructions, network):
 	count = 0
@@ -32,9 +37,26 @@ def traverse_network(instructions, network):
 		count += 1
 
 	return count
-		
+
+def traverse_multiple(instruction, network, start_nodes):
+	cycles = []
+	in_length = len(instructions)
+
+	for node in start_nodes:
+		steps = 0
+		while node[2] != 'Z':
+			direction = 0
+			if instruction[steps % in_length] == 'R':
+				direction = 1
+
+			node = network[node][direction]
+			steps += 1
+		cycles.append(steps)
+	
+	return math.lcm(*cycles)
+	
 FILE_PATH = 'input.txt'
 data = read_file(FILE_PATH)
-instructions, network = format_data(data)
-steps = traverse_network(instructions, network)
+instructions, network, start_nodes  = format_data(data)
+steps = traverse_multiple(instructions, network, start_nodes)
 print(steps)
